0f5193c42af6feea92c18b6b53e816ef837d7a1e
LincolnYang1 <lin.young@gmail.com>
2017-11-23 14:22:55 +0800

    Add files via upload

---
+++ .gitignore/src/WastonsAutoOrder/Program.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WastonsLib;
+
+namespace WastonsAutoOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = ConfigurationManager.AppSettings["Username"];
+            string pwd = ConfigurationManager.AppSettings["Password"];
+
+            var web = WastonsWebDriverExt.CreateWebDriver();
+            web.LoginWastons(username, pwd)
+                .Tee(w => Thread.Sleep(10000))
+                .Pipe(w => { w.Url = "https://www.watsons.com.sg/my-account/order/01076563"; return w; })
+
+                .Pipe(w => { w.FindElement(By.Id("addOrderAllEntryToBasket")).Click(); return w; })
+                .Tee(w => Thread.Sleep(10000))
+                .Pipe(w => { w.Url = "https://www.watsons.com.sg/checkoutstepone"; return w; })
+                .Pipe(w => { w.FindElement(By.Id("termsConditions")).Click(); return w; })
+                .Pipe(w => {
+                    w.FindElement(By.Id("mybasketCheckoutButton")).Click(); return w; })
+                .Pipe(w => { w.FindElement(By.Id("popupClose")).Click(); return w; })
+                .Tee(w => Thread.Sleep(1000))
+                .Pipe(w => { w.FindElement(By.Id("mybasketCheckoutButton")).Click(); return w; }).Tee(w => Thread.Sleep(1000))
+                .Pipe(w => { w.FindElement(By.Name("deliveryMode")).Click(); return w; }).Tee(w => Thread.Sleep(1000))
+                .Pipe(w => { w.FindElement(By.Name("radioResultItem")).Click(); return w; }).Tee(w => Thread.Sleep(1000))
+                .Pipe(w => { w.FindElement(By.ClassName("gotoNextStep ")).Click(); return w; })
+
+                .Pipe(w => { w.FindElement(By.Id("promotionInputId")).SendKeys("25OFF2311"); return w; })
+                .Pipe(w => { w.FindElement(By.Id("promotionButton")).Click(); return w; })
+                .Tee(w => Thread.Sleep(2000))
+
+                .Pipe(w => { w.FindElement(By.Id("redeemPointInputId")).SendKeys("2"); return w; })
+                .Pipe(w => { w.FindElement(By.Id("redeemButton")).Click(); return w; })
+                .Tee(w => Thread.Sleep(2000))
+                .Pipe(w => { w.FindElement(By.Id("gotoNextStepId")).Click(); return w; })
+
+                .Pipe(w => { w.FindElement(By.Name("name")).SendKeys(ConfigurationManager.AppSettings["CreditcardName"]); return w; })
+                .Pipe(w => { w.FindElement(By.Name("cardNo")).SendKeys(ConfigurationManager.AppSettings["CreditcardNo"]); return w; })
+                .Pipe(w => { w.FindElement(By.Name("cvv")).SendKeys(ConfigurationManager.AppSettings["CreditcardCVV"]); return w; })
+                .Pipe(w => { w.FindElement(By.Name("expiryMonth")).SendKeys(ConfigurationManager.AppSettings["CreditcardExpireMonth"]); return w; })
+                .Pipe(w => { w.FindElement(By.Name("expiryYear")).SendKeys(ConfigurationManager.AppSettings["CreditcardExpireYear"]); return w; })
+                .Pipe(w => { w.FindElement(By.Id("agree")).Click(); return w; })
+                .Tee(w => Thread.Sleep(2000))
+                .Pipe(w => { w.FindElement(By.Id("submit")).Click(); return w; });
+
+        }
+    }
+}
---
+++ .gitignore/src/WastonsLib/Extensions.cs
@@ -0,0 +1,106 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace WastonsLib
+{
+
+    public static class Extensions
+    {
+        public static IWebElement GetElement(this IWebDriver web, By by)
+        {
+            try
+            {
+                return web.FindElement(by);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static T Tee<T>(this T t, Action<T> act) { act(t); return t; }
+        public static R Pipe<T, R>(this T t, Func<T, R> func) => func(t);
+    }
+
+    public static class WastonsWebDriverExt
+    {
+        private const string NEXT_PAGE_CSS_STYLE = "sprite-P01B28NextSmStatic";
+        private const string WASTONS_PAGING_URL = "https://www.watsons.com.sg/my-account/orders?sort=byDate&page=@PAGENO&resultsForPage=10&sort=";
+        private const string WASTONS_LOGIN_URL = "https://www.watsons.com.sg/login";
+
+        public static IEnumerable<string> GetWastonPagingUrls(this IWebDriver web)
+        {
+            var i = 0;
+            while (web.GetElement(By.ClassName(NEXT_PAGE_CSS_STYLE)) != null)
+            {
+                i++;
+                yield return WASTONS_PAGING_URL.Replace("@PAGENO", i.ToString());
+            }
+        }
+
+        public static IEnumerable<string> GetWastonsInvoiceUrls(this IWebDriver web)
+        {
+            return
+                web.GetWastonPagingUrls()
+                .SelectMany(url =>
+                {
+                    web.Url = url;
+                    return web.FindElements(By.ClassName("bgDotRight"))
+                        .Where(td => td.Text == "Invoice")
+                        .Select(td => td.FindElement(By.TagName("a")))
+                        .Select(a => a.GetAttribute("href"))
+                        .ToList();
+                }
+                );
+        }
+
+        public static IWebDriver CreateWebDriver()
+        {
+            //FirefoxProfile profile = new FirefoxProfile();
+            //Proxy p = new Proxy();
+            //p.IsAutoDetect = true;
+            //profile.SetProxyPreferences(p);
+            //profile.SetPreference("services.sync.prefs.sync.browser.download.manager.showWhenStarting", false);
+            //profile.SetPreference("pdfjs.disabled", true);
+            //profile.SetPreference("print.always_print_silent", true);
+            //profile.SetPreference("print.show_print_progress", false);
+            //profile.SetPreference("browser.download.show_plugins_in_list", false);
+            //IWebDriver web = new FirefoxDriver(profile);
+            IWebDriver web = new ChromeDriver(@"C:\Tools\chromedriver_win32");
+            return web;
+        }
+
+        public static IWebDriver LoginWastons(this IWebDriver web, string username, string pwd)
+        {
+            web.Url = WASTONS_LOGIN_URL;
+            Thread.Sleep(1000);
+
+            web.FindElement(By.Id("j_username")).SendKeys(username);
+            web.FindElement(By.Id("j_password")).SendKeys(pwd);
+            web.FindElement(By.Id("signUpFormSumbitBtn")).Click();
+
+            web.FindElement(By.Id("myAccountfl"));
+            return web;
+        }
+
+        public static void SafePrint(IWebDriver web, string url)
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)web;
+
+            web.Url = FormatUrl(url);
+            Thread.Sleep(5000);
+            //js.ExecuteScript("window.print();");
+            Thread.Sleep(5000);
+        }
+
+        static string FormatUrl(string url)
+        {
+            return url.Substring(0, url.IndexOf("?"));
+        }
+    }
+}
